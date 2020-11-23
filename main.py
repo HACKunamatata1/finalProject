aac3db02d7eeaeadaa76957c3cbe5996a4a944bd
mirunaen <mirunaen@gmail.com>
2020-11-23 15:17:31 +0100

    teacher material

---
+++ main.py
@@ -0,0 +1,39 @@
+import pyxel
+
+class gridSquare:
+    #properties coordX and coordY,initialize 0,0
+    #global variables or @property
+
+
+class App:
+     board=[]
+    #Call creation process in an object
+    #Init process to declare variables,to set initial values
+    #images 16*16 pyxels
+    MYSIZE=16
+    HEIGHT=128
+    WIDTH=160
+    def __init__(self):
+        pyxel.init(160, 120, caption="Hello Pyxel")
+        pyxel.image(0).load(0, 0, "img/sprites.png")
+        for i in range (self.WIDTH/self.MYSIZE):
+            self.board.append([])
+            for j in range (self.HEIGHT/self.MYSIZE):
+                self.board[i].append([])
+
+
+        cell in my grid
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
+        pyxel.blt(0,0, 0, 0, 0, 15, 15)
+
+
+App()
