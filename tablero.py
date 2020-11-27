f90b9df01e4a80646a755f32b904a85392910dce
mirunaen <mirunaen@gmail.com>
2020-11-27 19:09:52 +0100

    tutorial

--- tablero.py
+++ tablero.py
@@ -1,22 +1,21 @@
 import pyxel
 from constants import *
 from cell import *
+import random
 
 
 class Maingame:
 
-    h = HEIGHT
-    w = 
-
+    WIDTH, HEIGHT = 256,256
     cursorX, cursorY = 16,16
-    cellroW=int(W/16)
+    cellroW=int(WIDTH/16)
     cellcolumn=int(HEIGHT/16)
     boardmatrix = []
     color = 0
-
+    lines=[]
     def __init__(self):
 
-        pyxel.init(self.W, self.HEIGHT, caption = "Pyxel Lemmings")
+        pyxel.init(self.WIDTH, self.HEIGHT, caption = "Pyxel Lemmings")
 
         for i in range (self.cellroW):
             self.boardmatrix.append([])
@@ -25,6 +24,15 @@ class Maingame:
                 cell.image = "v"
                 self.boardmatrix[i].append(cell)
     
+      
+        for i in range(3):
+            placed=True
+            while placed:
+                randomNum=random.randint(32,255)
+                if not randomNum in self.lines:
+                    self.lines.append(randomNum) 
+                    placed=False
+
         pyxel.run(self.update, self.draW)
 
 
@@ -35,7 +43,7 @@ class Maingame:
             pyxel.quit()
         #Movimientos rectangulo
         if pyxel.btnp(pyxel.KEY_RIGHT):
-            if self.cursorX+16<=self.W-16:
+            if self.cursorX+16<=self.WIDTH-16:
                 self.cursorX+=16
         if pyxel.btnp(pyxel.KEY_LEFT):
             if self.cursorX-16>=0:
@@ -44,7 +52,7 @@ class Maingame:
             if self.cursorY+16<=self.HEIGHT-16:
                 self.cursorY+=16
         if pyxel.btnp(pyxel.KEY_UP):
-            if self.cursorY-16>=0:
+            if self.cursorY-16>=32:
                 self.cursorY-=16
         if pyxel.btnp(pyxel.KEY_SPACE):
             position_in_roW = int(self.cursorY/16)
@@ -54,7 +62,13 @@ class Maingame:
     def draW(self):
         pyxel.cls(self.color)
         pyxel.rectb(self.cursorX, self.cursorY, 16, 16, 1)
-        
+        pyxel.line(0,32,255,32,10)
+        lines=[]
+        for i in self.lines:
+            pyxel.line(0,i,64,i,7)
+            
+
+
         for i in range(self.cellroW):            
             for j in range(self.cellcolumn):
                 cellcheck=self.boardmatrix[i][j]
